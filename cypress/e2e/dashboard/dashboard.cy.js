d466b1518dff3b825f6e2d71d725b7b33c2f59b9
marcelo.kade <marcelo.kade@gmail.com>
2026-05-10 04:53:06 -0300

    test: add dashboard test scenarios

---
+++ cypress/e2e/dashboard/dashboard.cy.js
@@ -0,0 +1,32 @@
+import { dashboardPage } from '../../../pages/dashboard/dashboard.page'
+
+describe('Dashboard', () => {
+
+    beforeEach(() => {
+        cy.visit('/')
+        cy.login()
+        cy.contains('Continuar').click()
+    })
+
+    it('Deve acessar o dashboard com sucesso', () => {
+        dashboardPage.url().should('include', '/dashboard')
+        dashboardPage.header().should('be.visible')
+    })
+
+    it('Deve exibir elementos principais do dashboard', () => {
+        dashboardPage.logo().should('be.visible')
+        dashboardPage.perfil().should('be.visible')
+        dashboardPage.sidebar().should('be.visible')
+    })
+
+    it('Deve permitir navegação pelo menu lateral', () => {
+        dashboardPage.abrirMenuLateral()
+        dashboardPage.url().should('include', '/campanha')
+    })
+
+    it('Deve navegar para perfil ao clicar em Candidato', () => {
+        dashboardPage.acessarPerfil()
+        dashboardPage.url().should('include', '/dashboard/perfil')
+    })
+
+})
